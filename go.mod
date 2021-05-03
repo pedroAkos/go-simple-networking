c8ba2b386fe7756e2a0911802070cd21f27e5381
pedroAkos <pah.costa@campus.fct.unl.pt>
2021-05-03 20:36:24 +0100

    configuration

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.7.0
+	github.com/spf13/viper v1.7.1
 )
---
+++ pkg/neti/configuration.go
@@ -0,0 +1,52 @@
+package neti
+
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+type Configuration struct {
+	iface 	string
+	ip		 	string
+	port 		int
+
+	buffSize	int
+}
+
+func (c Configuration) String() string {
+	return fmt.Sprintf("{iface: %v, ip: %v, port: %v, buffsize: %v}", c.iface, c.ip, c.port, c.buffSize)
+}
+
+func SetPFlags() {
+	pflag.String("net.ip", "", "IP address to bind")
+	pflag.String("net.iface", "lo0", "Network interface to bind")
+	pflag.Int("net.port", 10000, "Port to bind")
+}
+
+func LoadConfiguration(viper *viper.Viper) Configuration {
+	c := Configuration{}
+	c.ip = viper.GetString("net.ip")
+	c.port = viper.GetInt("net.port")
+	c.iface = viper.GetString("net.iface")
+
+	viper.SetDefault("buffSize", 1024)
+	c.buffSize = viper.GetInt("buffSize")
+
+	if c.ip == "" {
+		var err error
+		if c.ip, err = GetInterfaceIpv4Addr(c.iface); err != nil {
+			panic(err)
+		}
+	}
+
+	return c
+}
+
+func (c Configuration) Address() string {
+	return fmt.Sprintf("%s:%d", c.ip, c.port)
+}
+
+func (c Configuration) BuffSize() int {
+	return c.buffSize
+}
